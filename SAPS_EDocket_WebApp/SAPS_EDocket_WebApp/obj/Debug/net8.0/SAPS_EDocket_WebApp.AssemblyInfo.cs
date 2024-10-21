1261f3553b0e6f0be9576b2ed8ff5c7826374d5b
Bokamoso-max <bokamosoramadie@gmail.com>
2024-10-21 18:39:24 +0200

    Update

--- SAPS_EDocket_WebApp/SAPS_EDocket_WebApp/obj/Debug/net8.0/SAPS_EDocket_WebApp.AssemblyInfo.cs
+++ SAPS_EDocket_WebApp/SAPS_EDocket_WebApp/obj/Debug/net8.0/SAPS_EDocket_WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SAPS_EDocket_WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da8c3524312b6e8fad7556a9a48b07b836b17822")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1c9ac5afa309921d59b2882d08148c518b3952e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SAPS_EDocket_WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SAPS_EDocket_WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
