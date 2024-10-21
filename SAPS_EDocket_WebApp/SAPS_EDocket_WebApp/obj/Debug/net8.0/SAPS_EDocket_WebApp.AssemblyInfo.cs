136155c5e97dfd217514d594f0f7e40f852d0f0f
Bokamoso-max <bokamosoramadie@gmail.com>
2024-10-21 11:51:52 +0200

    Created login page
    Created the login page using html, css and javascript

--- SAPS_EDocket_WebApp/SAPS_EDocket_WebApp/obj/Debug/net8.0/SAPS_EDocket_WebApp.AssemblyInfo.cs
+++ SAPS_EDocket_WebApp/SAPS_EDocket_WebApp/obj/Debug/net8.0/SAPS_EDocket_WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SAPS_EDocket_WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3b492590fd980a7e03b510e4c1fb23c9d5c910b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c89cd6f51aa0b7dd57b9efc79e26e8c4a615e738")]
 [assembly: System.Reflection.AssemblyProductAttribute("SAPS_EDocket_WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SAPS_EDocket_WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
